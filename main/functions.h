745c21995bffd6cf9aba77c74b0e198f5e6a41cd
jrsorber <jrsorber@ncsu.edu>
2024-08-31 22:51:58 -0400

    cleaned, no wifi

---
+++ main/functions.h
@@ -0,0 +1,18 @@
+/*
+This file holds the function prototypes for the entire project
+functions.h should be included in all .c source files
+
+Author:         James Sorber
+Contact:        jrsorber@ncsu.edu
+Created:        8/31/2024
+Modified:       -
+Last Built With ESP-IDF v5.2.2
+*/
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+// SOFTAP.C
+void wifi_init_softap(void);
+
+#endif
--- main/main.c
+++ main/main.c
@@ -23,6 +23,8 @@
 #include "lwip/sys.h"
 #include <lwip/netdb.h>
 
+#include "functions.h"
+
 
 #define PORT                        7983
 #define KEEPALIVE_IDLE              5
@@ -141,17 +143,33 @@ CLEAN_UP:
     vTaskDelete(NULL);
 }
 
-void app_main(void)
-{
+void app_main(void){
+    ESP_LOGI(TAG, "Entry Point");
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+
+    // //Initialize NVS
+    // esp_err_t ret = nvs_flash_init();
+    // if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    //   ESP_ERROR_CHECK(nvs_flash_erase());
+    //   ret = nvs_flash_init();
+    // }
+    // ESP_ERROR_CHECK(ret);
+    // ESP_LOGI(TAG, "NVS Done");
+
+    // ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
+    // wifi_init_softap();
+
 
     /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
      * Read "Establishing Wi-Fi or Ethernet Connection" section in
      * examples/protocols/README.md for more information about this function.
      */
-    ESP_ERROR_CHECK(example_connect());
+    //ESP_ERROR_CHECK(example_connect());
 
-    xTaskCreate(tcp_server_task, "tcp_server", 4096, (void*)AF_INET, 5, NULL);
+    xTaskCreate(tcp_server_task, "tcp_server", 8192, (void*)AF_INET, 5, NULL);
+    ESP_LOGI(TAG, "Exit app_main");
+    return;
 }
